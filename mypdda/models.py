d7e2193e11f05e1c266b775bc88a91726b7067de
Muhammad Basit Latif <70871988+mbasitshah@users.noreply.github.com>
2021-01-31 00:20:39 +0500

    Update models.py

--- mypdda/models.py
+++ mypdda/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Results(models.Model):
+    PlantName = models.CharField(max_length=200, null=True)
+    Condition = models.CharField(max_length=200, null=True)
+    DiseaseName = models.CharField(max_length=200, null=True)
+    Medicines = models.CharField(max_length=200, null=True)
+    Precautions = models.CharField(max_length=200, null=True)
